388f72df866d386b79f00ef2aa572993970e014e
Wayward One <waywardone@gmail.com>
2019-09-10 21:18:46 -0700

    Initial commit of PowerShell renamer and README

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # garmin-fit-rename
-Rename FIT files from Garmin watches to a more human readable form
+
+
+## About
+
+I've used Garmin devices to track my physical activities for a number of years and am generally happy with them for that purpose. I have been annoyed by the file naming conventions used when data is exported from the devices. This is a utility that renames the FIT files to use a more human readable name that provides a chronological context at a glance. Currently, it handles file naming conventions from two models that I have access to - a Forerunner 235 and a Forerunner 645.
+
+
+
+## Usage
+
+
+```sh
+.\garmin-fit-rename.ps1 -SRCDIR C:\Path\To\Directory\With\FITFiles
+```
+
+
+
+## Contributing
+
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+
+
+## Contact
+
+Wayward One - waywardone@gmail.com
+
+Project Link: [https://github.com/waywardone/garmin-fit-rename](https://github.com/waywardone/garmin-fit-rename)
+
+
+
+## Acknowledgements
+* [Garmin Forums](https://forums.garmin.com/apps-software/mac-windows-software/f/garmin-express-windows/81739/change-file-name-format)
+* [BASH implementation linked to by Garmin Forums user admin8081](https://www.dropbox.com/s/16frs902kz17uzg/copy-garmin-fit-files-2018-01-05.bash?dl=0)
+
---
+++ garmin-fit-rename.ps1
@@ -0,0 +1,117 @@
+# Links to original information that led to this script:
+# https://forums.garmin.com/apps-software/mac-windows-software/f/garmin-express-windows/81739/change-file-name-format
+# https://www.dropbox.com/s/16frs902kz17uzg/copy-garmin-fit-files-2018-01-05.bash?dl=0
+
+param (
+    [Parameter(Mandatory=$True)]
+    [string] $SRCDIR
+)
+
+function sanityCheck
+{
+    if ( [string]::IsNullOrEmpty($SRCDIR) )
+    {
+        Write-Host "Source directory for Garmin FIT files not specified!" -Foregroundcolor Red -Backgroundcolor Black
+        exit
+    }
+
+    if ( ! ( Test-Path -path $SRCDIR ) )
+    {
+        Write-Host "$SRCDIR doesn't exist. Specify a valid SRCDIR for Garmin FIT files." -Foregroundcolor Red -Backgroundcolor Black
+        exit
+    }
+}
+
+function FR645
+{
+    param (
+        [Parameter(Mandatory=$True)]
+        [string] $name = ""
+    )
+    
+    $NEWNAME = $name -replace "(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)\.[a-zA-Z]*",'$1$2$3-$4$5$6.fit'
+    return $NEWNAME
+}
+
+function FR235
+{
+    param(
+        [Parameter(Mandatory=$True)]
+        [string] $name
+    )
+
+    $GCHARMAP = @{
+        '0' = 0;    '1' = 1;    '2' = 2;    '3' = 3;
+        '4' = 4;    '5' = 5;    '6' = 6;    '7' = 7;
+        '8' = 8;    '9' = 9;    'A' = 10;    'B' = 11;
+        'C' = 12;    'D' = 13;    'E' = 14;    'F' = 15;
+        'G' = 16;    'H' = 17;    'I' = 18;    'J' = 19;
+        'K' = 20;    'L' = 21;    'M' = 22;    'N' = 23;
+        'O' = 24;    'P' = 25;    'Q' = 26;    'R' = 27;
+        'S' = 28;    'T' = 29;    'U' = 30;    'V' = 31;
+        'W' = 32;    'X' = 33;    'Y' = 34;    'Z' = 35;
+      }
+
+    $CENTURY = get-date -UFormat %C
+    $NEWNAME = "$CENTURY"
+    $n = 0
+    while($n -lt 8) {
+        $GCHAR = $name.SubString($n,1)
+        $NCHAR = $GCHARMAP.$GCHAR
+
+        if ( $n -eq 3 ) {
+            $NEWNAME = $NEWNAME + '-'
+        }
+
+        if ( $n -eq 0 -and $NCHAR -lt 10 ) {
+            $NEWNAME = $NEWNAME + "1$NCHAR"
+        } elseif ( ($n -eq 1 -or $n -eq 2 -or $n -eq 3) -and $NCHAR -lt 10 ) {
+            $NEWNAME = $NEWNAME + "0$NCHAR"
+        } elseif ( $n -eq 4 -or $n -eq 5 -or $n -eq 6 -or $n -eq 7 ) {
+            $NEWNAME = $NEWNAME + $GCHAR
+        } else {
+            $NEWNAME = $NEWNAME + $NCHAR
+        }
+        $n = $n + 1
+    }
+    $NEWNAME = $NEWNAME + '-' + $name.SubString(0,8) + '.fit'
+    return $NEWNAME
+}
+
+$CURRUSER = [Environment]::UserName
+$DESKTOP = "C:\Users\$CURRUSER\Desktop"
+
+$TSTAMP = get-date -f yyyyMMdd-HHmmss
+$DESTDIR = "$DESKTOP\$TSTAMP-Garmin"
+
+sanityCheck
+
+if(!(Test-Path -Path $DESTDIR )){
+  New-Item -Force -ItemType directory -Path $DESTDIR
+}
+
+$files = Get-ChildItem -Path $SRCDIR -Recurse -Include *.fit,*.FIT
+
+foreach ($f in $files) {
+    $file = Get-Item $f
+    $fext = $file.Extension
+    $fname = $file.Name
+
+    if ($fname -match "\d{4}-\d{2}-\d{2}-\d{2}-\d{2}-\d{2}\.fit") {
+        # Example: 2019-09-01-06-40-57.fit
+        $NEWNAME = FR645($fname)
+    } elseif ($fname -match "\d{1}[1-9ABC]{1}[1-9A-V]{1}[1-9A-N]{1}[\d]{4}") {
+        # Example: 96FG1906.FIT
+        $NEWNAME = FR235($fname)
+    } else {
+        Write-Host "Don't know how to process $f" -Foregroundcolor Red -Backgroundcolor Black
+        continue
+    }
+    
+    $Y = $NEWNAME.SubString(0,4)
+    if(!(Test-Path -Path $DESTDIR\$Y )){
+        New-Item -Force -ItemType directory -Path $DESTDIR\$Y
+    }
+    Write-Host "Renaming " $fname "to " $NEWNAME
+    Copy-Item -Path $f -Destination $DESTDIR\$Y\$NEWNAME
+}
